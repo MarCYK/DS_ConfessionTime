fdc1a1017894cd819874189b53f718a2d2a780cd
irfanmarsidek <92704052+irfanmarsidek@users.noreply.github.com>
2022-06-22 01:59:57 +0800

    las

--- src/main/java/User/RunConfession.java
+++ src/main/java/User/RunConfession.java
@@ -22,11 +22,11 @@ public class RunConfession extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 //        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Submission.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/welcomePage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/demo.fxml"));
 
         Scene scene = new Scene(root);
 //        scene.getStylesheets().add("/styles/submission.css");
-        scene.getStylesheets().add("/styles/welcomepage.css");
+        scene.getStylesheets().add("/styles/demo.css");
 	stage.setResizable(false);
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
