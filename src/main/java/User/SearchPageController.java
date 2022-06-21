20d188ef70d20585ebbe592f4d509851bb9794c3
Marvin Chin Yi Kai <u2000490@siswa.um.edu.my>
2022-06-21 16:45:44 +0800

    fck this x2

--- src/main/java/User/SearchPageController.java
+++ src/main/java/User/SearchPageController.java
@@ -1,91 +1,60 @@
 package User;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-
-
-import Confession.Post;
 import Confession.showPost;
-import Engine.Search;
 import SQLOperations.operationTest;
-import SQLOperations.timeClass;
-import java.awt.Button;
-import java.awt.Label;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import java.sql.*;
-
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
 /**
  * FXML Controller class
  *
- * @author User
+ * @author Marvin Chin Yi Kai
  */
 public class SearchPageController implements Initializable {
-    
-    @FXML
-    private ImageView image_Return;
-    
+
     @FXML
     private TextField textField_promptInput;
-    
     @FXML
-    private TableView table_Search;
-    
+    private TableView<showPost> table_Search;
     @FXML
-    private TableColumn column_ID;
-    
+    private TableColumn<showPost, String> column_ID;
     @FXML
-    private TableColumn column_Content;
-    
+    private TableColumn<showPost,String> column_Content;
     @FXML
-    private TableColumn column_Time;
-    
-    operationTest sql = new operationTest();
-    Connection conn = sql.getConnection();
-    ObservableList<showPost> ob = FXCollections.observableArrayList();
+    private TableColumn<showPost, String> column_Time;
+    @FXML
+    private ImageView image_Return;
 
-    //Search sr = new Search();
-    
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("ini");
-        Runn();
-    }
-    
-    @FXML
-    private void Runn() {
+        operationTest sql = new operationTest();
+        Connection conn = sql.getConnection();
+        ObservableList<showPost> ob = FXCollections.observableArrayList();
         try{
-            Connection conn = sql.getConnection();
             ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM node");
             
             while(rs.next()){
@@ -143,8 +112,8 @@ public class SearchPageController implements Initializable {
             Logger.getLogger(SearchPageController.class.getName()).log(Level.SEVERE,null,e);
             e.printStackTrace();
         }
-    }
-    
+    }    
+
     @FXML
     private boolean BackToUserInterface() throws ClassNotFoundException {
         Stage mainStage = (Stage) image_Return.getScene().getWindow();
@@ -160,5 +129,3 @@ public class SearchPageController implements Initializable {
     }
     
 }
-    
-
