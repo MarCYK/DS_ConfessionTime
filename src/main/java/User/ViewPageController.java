fefad78b131e1920a30763178ebc12f9fd2041ea
26Jon <jonathanwong1214@gmail.com>
2022-06-21 18:51:11 +0800

    Testing code for automatically increasing the number of content based on number of node in database

--- src/main/java/User/ViewPageController.java
+++ src/main/java/User/ViewPageController.java
@@ -5,7 +5,9 @@ package User;
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
+import SQLOperations.operationTest;
 import java.net.URL;
+import java.sql.Connection;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +15,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -26,16 +31,36 @@ public class ViewPageController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
     
+    @FXML
+    private ImageView image_Return;
     @FXML
     private Button button_Reaction;
-    
     @FXML
     private Button button_Reply;
+    @FXML
+    private Label label_ID;
+    @FXML
+    private Label label_DateTime;
+    @FXML
+    private Label label_ReplyingToID;
+    @FXML
+    private ScrollPane Pane_Scrolling;
+    @FXML
+    private TextArea [] textarea_Content;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        operationTest sql = new operationTest();
+        Connection conn = sql.getConnection();
+        
+        
+        int label_count = sql.count("node", conn);
+        TextArea [] textarea_Content = new TextArea[label_count];
+        for(int i = 0; i < label_count; i++){
+            Pane_Scrolling.setContent(textarea_Content[i]);
+        }
+    }
     
     @FXML
     private boolean Reacting() throws ClassNotFoundException {
