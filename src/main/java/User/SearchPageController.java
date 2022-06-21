9608f9160ba8d3d06a808ff855f253e08516a90f
irfanmarsidek <92704052+irfanmarsidek@users.noreply.github.com>
2022-06-22 06:13:17 +0800

    aa

--- src/main/java/User/SearchPageController.java
+++ src/main/java/User/SearchPageController.java
@@ -2,6 +2,7 @@ package User;
 
 import Confession.showPost;
 import SQLOperations.operationTest;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -17,6 +18,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
@@ -239,17 +241,17 @@ public class SearchPageController implements Initializable {
     }    
 
     @FXML
-    private boolean BackToUserInterface() throws ClassNotFoundException {
-        Stage mainStage = (Stage) image_Return.getScene().getWindow();
+    private void BackToUserInterface(MouseEvent event) throws ClassNotFoundException, IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/UserInterface.fxml"));
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("UserInterface.fxml"));
-            Scene scene = new Scene(root);
-            mainStage.setScene(scene);
-            mainStage.setTitle("Test Window");
-        }
-            catch(Exception e){}
-        return true;
+		Scene scene = new Scene(root);
+	//        scene.getStylesheets().add("/styles/submission.css");
+		scene.getStylesheets().add("/styles/userinterface.css");
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+		stage.setTitle("JavaFX and Maven");
+		stage.setScene(scene);
+		stage.show();
     }
 
     @FXML
--- src/main/java/User/UserInterfaceController.java
+++ src/main/java/User/UserInterfaceController.java
@@ -5,13 +5,16 @@ package User;
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -22,6 +25,13 @@ import javafx.stage.Stage;
  * @author User
  */
 public class UserInterfaceController implements Initializable {
+
+    @FXML
+    private Label txt_submit;
+    @FXML
+    private Label Txt_ViewPage;
+    @FXML
+    private Label txt_Search;
 
     /**
      * Initializes the controller class.
@@ -41,47 +51,58 @@ public class UserInterfaceController implements Initializable {
     private ImageView image_Search;
     
     @FXML
-    private boolean ToSubmitPage() throws ClassNotFoundException {
-        Stage mainStage = (Stage) image_Submit.getScene().getWindow();
+    private void ToSubmitPage(MouseEvent event) throws ClassNotFoundException, IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/SubmitPage.fxml"));
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("SubmitPage.fxml"));
-            Scene scene = new Scene(root);
-            mainStage.setScene(scene);
-            mainStage.setTitle("Confession Time!");
-        }
-            catch(Exception e){}
-        return true;
+		Scene scene = new Scene(root);
+	//        scene.getStylesheets().add("/styles/submission.css");
+		scene.getStylesheets().add("/styles/submitpage.css");
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+		stage.setTitle("JavaFX and Maven");
+		stage.setScene(scene);
+		stage.show();
+        
     }
     
     @FXML
-    private boolean ToViewPage() throws ClassNotFoundException {
-        Stage mainStage = (Stage) image_View.getScene().getWindow();
+    private void ToViewPage(MouseEvent event) throws ClassNotFoundException, IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/demo.fxml"));
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("Demo.fxml"));
-            Scene scene = new Scene(root);
-            mainStage.setScene(scene);
-            mainStage.setTitle("Confession Time!");
-        }
-            catch(Exception e){}
-        return true;
+		Scene scene = new Scene(root);
+	//        scene.getStylesheets().add("/styles/submission.css");
+		scene.getStylesheets().add("/styles/demo.css");
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+		stage.setTitle("JavaFX and Maven");
+		stage.setScene(scene);
+		stage.show();
     }
     
-     private boolean ToSearchPage() throws ClassNotFoundException {
-        Stage mainStage = (Stage) image_Search.getScene().getWindow();
+    @FXML
+     private void ToSearchPage(MouseEvent event) throws ClassNotFoundException, IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/SearchPage.fxml"));
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("SearchPage.fxml"));
-            Scene scene = new Scene(root);
-            mainStage.setScene(scene);
-            mainStage.setTitle("Confession Time!");
-        }
-            catch(Exception e){}
-        return true;
+		Scene scene = new Scene(root);
+	//        scene.getStylesheets().add("/styles/submission.css");
+		scene.getStylesheets().add("/styles/userpage.css");
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+		stage.setTitle("JavaFX and Maven");
+		stage.setScene(scene);
+		stage.show();
     }
 
     @FXML
-    private void ToSearchInterface(MouseEvent event) {
+    private void submitPageText(MouseEvent event) {
     }
+
+    @FXML
+    private void ViewPageText(MouseEvent event) {
+    }
+
+    @FXML
+    private void SearchPageText(MouseEvent event) {
+    }
+
 }
